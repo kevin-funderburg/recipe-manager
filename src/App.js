4aded805a1a15cddd1dcef7115a69f3d17eae723
kevin-funderburg <ksfunderburg@icloud.com>
2022-03-31 18:22:27 -0500

    added newRecipe stub

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Login from './modules/authenticate/login/login';
-import EssayForm from './components/EssayForm'
 
 function App() {
   return (
     <div className="App">
-      {/*<Login/>*/}
-        EssayForm
+      <Login/>
     </div>
   );
 }
--- src/model/User.java
+++ src/model/User.java
@@ -12,8 +12,27 @@ public class User {
     String mailingAddress;
     String password;
     public List<Recipe> recipes = new ArrayList<>();
-    
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
+}
+
+
+public boolean userExists(String email) {
+    boolean exists = false;
+    String sql = "SELECT * FROM Users WHERE email = '" + email + "'";
+    //TODO execute SQL
+    return exists;
+}
+
+
+public addToDB(User user) {
+    String sql = "INSERT INTO Users (firstName, lastname, ";
 }
