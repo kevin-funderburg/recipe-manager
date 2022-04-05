edf8863feb8546a79785107fd560f59f7d2749c2
kevin-funderburg <ksfunderburg@icloud.com>
2022-04-05 10:54:34 -0500

    trying out formik

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Login from './modules/authenticate/login/login';
 import NewRecipe from './modules/forms/newRecipe/newRecipe'
+import Basic from './modules/forms/basic/basic'
 
 function App() {
   return (
     <div className="App">
-      <NewRecipe/>
+      <Basic/>
     </div>
   );
 }
--- src/modules/forms/newRecipe/newRecipe.jsx
+++ src/modules/forms/newRecipe/newRecipe.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import './newRecipe.css';
 import { TextField, Button} from '@mui/material';
+import { Formik } from 'formik';
 
 function NewRecipe(){
     return (
         <div className="mainContainer">
-            <div className="centerPane" display="block">
-                    <p>Sign In</p>
-                    <hr width="90%"/>
-                    <p>User Name:</p>
-                    <TextField className="textboxSize" placeholder="Enter username"></TextField>
-                    <p>Password:</p>
-                    <TextField className="textboxSize"placeholder="Enter password"></TextField>
-                    <br/><br/>
-                    <Button variant='contained' color='primary'>SIGN IN</Button>
-                    <br/><br/>
-                    <a href=''>Forgot password?</a>
-                </div>
+            <div className="centerPane">
+                <div className="formWindow" display="block">
+                        <p>New Recipe</p>
+                        <hr width="90%"/>
+                        <p>User Name:
+                        <TextField className="textboxSize" placeholder="Enter username"></TextField>
+                        </p>
+                        <p>Password:</p>
+                        <TextField className="textboxSize"placeholder="Enter password"></TextField>
+                        <br/><br/>
+                        <Button variant='contained' color='primary'>CANCEL</Button>
+                        <br/><br/>
+                        <Button variant='contained' color='primary'>DOWNLOAD</Button>
+                        <br/><br/>
+                        <Button variant='contained' color='primary'>SAVE</Button>
+                        <br/><br/>
+                        <a href=''>Forgot password?</a>
+                    </div>
+            </div>
             </div>
     );
 }
