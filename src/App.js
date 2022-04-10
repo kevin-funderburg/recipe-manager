0e6d2acfe888d438ea572787381f4423d7dce4aa
Jessica Guo <jessicaguo@Jessicas-MBP.attlocal.net>
2022-04-10 15:26:36 -0500

    initial account manager

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Login from './modules/authenticate/login/login';
 import NewRecipe from './modules/forms/newRecipe/newRecipe'
 import Basic from './modules/forms/basic/basic'
 import Download from './modules/forms/download/download'
+import AcctManager from './modules/forms/acctManager/acctManager'
 
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
-      {/* <Jessica/> */}
-      <NewRecipe/>
+      {/* <NewRecipe/> */}
       {/* <Download/> */}
+      <AcctManager/>
     </div>
   );
 }
