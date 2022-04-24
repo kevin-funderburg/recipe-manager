46c7267e3942a7619525061793f7dc8f9cb290b2
Jessica <jonnaxi22@gmail.com>
2022-04-24 01:41:48 -0500

    change style

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import Navbar from './modules/navbar'
 import RecipeList from './modules/forms/RecipeList/RecipeList'
 import AcctManager from './modules/forms/acctManager'
 
-import { BrowserRouter as Router, 
-         Route,
-         Routes, 
-         Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
