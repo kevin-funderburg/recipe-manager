8eaa68ded0f07075ac2ce911204d6c9270b9ae6b
kevin-funderburg <ksfunderburg@icloud.com>
2022-04-28 14:40:00 -0500

    added beautiful soup to help parse

---
+++ src/lib/download.py
@@ -0,0 +1,26 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import requests
+
+
+
+def getHTML(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    fp = urlopen(req,timeout=10)
+    mybytes = fp.read()
+
+    mystr = mybytes.decode("utf8")
+    fp.close()
+
+    return mystr
+
+def main():
+    print("Hello world")
+    URL = 'https://www.foodnetwork.com/recipes/oven-baked-salmon-recipe-1911951'
+    page = requests.get(URL)
+    # html = getHTML('https://www.foodnetwork.com/recipes/oven-baked-salmon-recipe-1911951')
+    soup = BeautifulSoup(page.content, "html.parser")
+    # print(html)
+
+if __name__ == "__main__":
+    main()
